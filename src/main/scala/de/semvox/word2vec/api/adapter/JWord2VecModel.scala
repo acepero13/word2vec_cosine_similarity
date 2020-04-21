4bc5c70ca5c5082ef76b9fd8f3181e7cafbd91bb
cepero <cepero@semvox.de>
2020-04-21 10:40:52 +0200

    [FEATURE] Remove default value in rank

--- src/main/scala/de/semvox/word2vec/api/adapter/JWord2VecModel.scala
+++ src/main/scala/de/semvox/word2vec/api/adapter/JWord2VecModel.scala
@@ -6,5 +6,5 @@ trait JWord2VecModel {
 
   def rank(word: java.lang.String,
            in: java.util.Set[java.lang.String],
-           N: java.lang.Integer = 40): java.util.Map[java.lang.String, java.lang.Float]
+           N: java.lang.Integer ): java.util.Map[java.lang.String, java.lang.Float]
 }
