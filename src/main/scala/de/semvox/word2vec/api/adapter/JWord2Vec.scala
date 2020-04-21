a5f4e4df0ca14c363f9a4f380050102d6a4b2f1a
Alvaro <acepero13@gmail.com>
2020-04-21 09:40:01 +0200

    [BUGFIX] Keep sorting in java interface

--- src/main/scala/de/semvox/word2vec/api/adapter/JWord2Vec.scala
+++ src/main/scala/de/semvox/word2vec/api/adapter/JWord2Vec.scala
@@ -6,14 +6,17 @@ import de.semvox.word2vec.api.Word2VecModel
 
 import scala.collection.JavaConversions._
 import scala.collection.JavaConverters._
+import scala.collection.mutable
 
 case class JWord2Vec(adapter: Word2VecModel) extends JWord2VecModel {
 
   override def relatedTopicsFor(sentence: util.List[String], possibleTopics: util.Set[String]): util.Map[String, lang.Float] = {
-    adapter.relatedTopicsFor(sentence, possibleTopics.toSet).map(r => r._1 -> float2Float(r._2)).toMap.asJava
+    val relatedTopics = adapter.relatedTopicsFor(sentence, possibleTopics.toSet).map(r => r._1 -> float2Float(r._2))
+    mutable.LinkedHashMap(relatedTopics:_*)
   }
 
   override def rank(word: String, in: util.Set[String], N: Integer): util.Map[String, lang.Float] = {
-    adapter.rank(word, in.toSet, N).map(r => r._1 -> float2Float(r._2)).toMap.asJava
+    val ranked = adapter.rank(word, in.toSet, N).map(r => r._1 -> float2Float(r._2))
+    mutable.LinkedHashMap(ranked:_*)
   }
 }
