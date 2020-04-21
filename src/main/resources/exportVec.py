b735263511f210be5b5f8d88a5b841c6825a96f7
Alvaro <acepero13@gmail.com>
2020-04-21 08:58:44 +0200

    [FEATURE] Refactor python exporter

--- src/main/resources/exportVec.py
+++ src/main/resources/exportVec.py
@@ -13,19 +13,23 @@ def load_vectors(fname):
     fin = io.open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
     n, d = map(int, fin.readline().split())
     data = {}
+    insertIntoDb(c, conn, fin)
+    conn.close()
+    return data
+
+
+def insertIntoDb(c, conn, fin):
     counter = 0
     for line in fin:
         tokens = line.rstrip().split(' ')
         vectors = " ".join(tokens[1:])
-        if(tokens[0]) == "'":
+        if (tokens[0]) == "'":
             continue
         c.execute("INSERT INTO embeddings VALUES ('" +
                   tokens[0] + "','" + vectors + "' )")
         if counter % 100 == 0:
             conn.commit()
         counter += 1
-    conn.close()
-    return data
 
 
 if __name__ == "__main__":
